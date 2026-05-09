a1612c23d196f86c7ef8ac372665a05e93c10c47
Dolan Miu <>
2026-05-10 00:13:06 +0100

    Upgrade packages
    Organise files

--- packages/runtime/src/public-api.ts
+++ packages/runtime/src/public-api.ts
@@ -2,12 +2,12 @@
  * Public API Surface of @blotch/angular-lynx
  */
 
-export { LynxLocationStrategy } from './lib/lynx-location-strategy';
 export { LynxLoggerService } from './lib/lynx-logger.service';
-export { LynxPlatformLocation } from './lib/lynx-platform-location';
-export { LynxRouteReuseStrategy } from './lib/lynx-route-reuse-strategy';
-export { provideLynxRouter } from './lib/lynx-router';
 export { provideLynxRenderer } from './lib/providers';
+export { LynxLocationStrategy } from './lib/router/lynx-location-strategy';
+export { LynxPlatformLocation } from './lib/router/lynx-platform-location';
+export { LynxRouteReuseStrategy } from './lib/router/lynx-route-reuse-strategy';
+export { provideLynxRouter } from './lib/router/lynx-router';
 export { bootstrapLynxApplication } from './lib/runtime';
 import './lib/types/runtime-types';
 import './lib/types/lynx';
